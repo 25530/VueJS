f97b7c02151a7978642c554dbbca794508d469b4
Sam Harwig <harwigsam@gmail.com>
2020-02-01 01:04:11 +0100

    finishing

---
+++ src/components/WishItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wish-item" v-bind:class="{'is-complete':wish.completed}">
+    <p>
+    <input type="checkbox" v-on:change="markComplete">
+    {{wish.title}}
+    <button @click="$emit('del-wish', wish.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WishItem",
+  props: ["wish"],
+  methods: {
+    markComplete() {
+      this.wish.completed = !this.wish.completed;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wish-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
---
+++ src/components/Wishes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="wish.id" v-for="wish in wishes">
+      <WishItem v-bind:wish="wish" v-on:del-wish="$emit('del-wish', wish.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import WishItem from './WishItem.vue';
+
+export default {
+  name: "Wishes",
+  components: {
+    WishItem
+  },
+  props: ["wishes"]
+}
+</script>
+
+<style scoped>
+
+</style>
